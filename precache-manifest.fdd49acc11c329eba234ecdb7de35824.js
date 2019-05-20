99fb65b77efd224e539130ef6fc65cc7863017d5
kaisukez <zykigix@hotmail.com>
2019-05-20 18:15:23 +0700

    update build

---
+++ precache-manifest.fdd49acc11c329eba234ecdb7de35824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46429bdb0ae31e53b6ad8c9a2dbb2f4f",
+    "url": "/max-stat/index.html"
+  },
+  {
+    "revision": "517c54e71b233649eb6a",
+    "url": "/max-stat/static/css/main.3f241e15.chunk.css"
+  },
+  {
+    "revision": "d12ccb2d12e7c752f26a",
+    "url": "/max-stat/static/js/2.887da76b.chunk.js"
+  },
+  {
+    "revision": "517c54e71b233649eb6a",
+    "url": "/max-stat/static/js/main.ad2d6b7d.chunk.js"
+  },
+  {
+    "revision": "0f9df091f88260367506",
+    "url": "/max-stat/static/js/runtime~main.6c5d5972.js"
+  }
+]);
